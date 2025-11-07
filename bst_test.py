0cfce198564ee40c994a4ff882b3794e06a6e5b3
alexchang319 <alexchang319@outlook.com>
2025-11-06 16:14:08 -0800

    Added tests

--- bst_test.py
+++ bst_test.py
@@ -5,6 +5,14 @@ from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 from bst import *
 
+testtree=BinarySearchTree(comes_before_lessthan,Node(5,
+                                                    left =  Node(3,
+                                                        left = Node(2, None, None),
+                                                        right = Node(4, None, None)),
+                                                    right = Node(7,
+                                                        left = Node(6, None, None),
+                                                        right = None)))
+
 class BSTTests(unittest.TestCase):
     #insert
     def test_insert1(self):
@@ -33,8 +41,14 @@ class BSTTests(unittest.TestCase):
                                                          right = Node(7, None, None)))
         self.assertEqual(insert(og_bst, 7), expected_bst)
 
+    def test_delete(self):
+       self.assertEqual(delete(testtree,5).tree, Node(val=4, left=Node(val=3, left=Node(val=2, left=None, right=None), right=None), right=Node(val=7, left=Node(val=6, left=None, right=None), right=None)))
     #idk how to make test functions for random_tree
 
+    def test_lookup(self):
+       self.assertEqual(lookup(testtree,0),False)
+       self.assertEqual(lookup(testtree,2),True)
+           
     def test_example(self):
         pass
 
